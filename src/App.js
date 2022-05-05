74cb8ebf574d63e0451bfe800f48b37b79ecb596
Brs545 <baris.candoganb@hotmail.com>
2022-05-06 02:07:42 +0300

    creating user interface

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React from 'react'
 import './App.css'
+import Post from './Post.js'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className='app'>
+      <div className='app__header'>
+        <img
+          className='app__headerImage'
+          src='https://marka-logo.com/wp-content/uploads/2020/04/Instagram-Logo.png'
+          alt=''
+        />
+      </div>
+      <h1>asjdnsajkdsan</h1>
+      <Post
+        username='candoganbaris '
+        caption='WOW it works'
+        imageURL='https://reactjs.org/logo-og.png'
+      />
+      <Post
+        username='kagancandogan '
+        caption='Awsome !'
+        imageURL='https://www.gstatic.com/devrel-devsite/prod/vd881210f9673d9985aa8864f5fd9cbfbb5e2b56bbc9c6e51f369462d48e84ef9/firebase/images/touchicon-180.png'
+      />
+      <Post
+        username='gulsunn'
+        caption='That is dope'
+        imageURL='https://static2.elcorreo.com/www/multimedia/202002/07/media/cortadas/kobe-bryant-kYzC-U10073264255kQE-624x485@El%20Correo.jpg'
+      />
+    </div>
+  )
 }
 
 export default App
