058a68b316b0e7854147193fcbd55b21d1342ab0
Brs545 <baris.candogan@hotmail.com>
2022-05-07 01:08:15 +0300

    implement firebase database

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/firestore'
 import 'firebase/compat/auth'
-import 'firebase/compat/storage' // <----
+import 'firebase/compat/storage'
 
 const firebaseApp = firebase.initializeApp({
   apiKey: 'AIzaSyBbsyAggyhH3pKcWjRzZ3ZWuAnose0BAAQ',
